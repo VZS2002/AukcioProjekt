463af2307fcd4563dee5f58c90b77d784564e9a9
Varga <vargazsigmond02@gmail.com>
2022-09-22 10:29:53 +0200

    Eddig jutottam a gyakorlat alatt

---
+++ src/hu/petrik/aukcio/Festmenyek.java
@@ -0,0 +1,12 @@
+package hu.petrik.aukcio;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Festmenyek {
+    private List<Festmeny> lista;
+
+    public Festmenyek(Festmeny[] festmenyekTomb) {
+
+    }
+}
--- src/hu/petrik/aukcio/Main.java
+++ src/hu/petrik/aukcio/Main.java
@@ -1,14 +1,25 @@
 package hu.petrik.aukcio;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        List<Festmeny> lista= new ArrayList<Festmeny>();
         Festmeny festmeny1 = new Festmeny("asd1","asd2","asd3");
-        System.out.println(festmeny1);
-        festmeny1.licit();
-        System.out.println(festmeny1);
-        festmeny1.licit();
-        System.out.println(festmeny1);
-        festmeny1.licit();
+        Festmeny festmeny2 = new Festmeny("Elfolyó idő","Salvador Dalí","asd3");
+
+        lista.add(festmeny1);
+        lista.add(festmeny2);
+
+        System.out.println("Adj meg egy darabszámot");
+        int db= sc.nextInt();
+
+        for (int i = 0; i < db; i++) {
+            
+        }
     }
 
 
