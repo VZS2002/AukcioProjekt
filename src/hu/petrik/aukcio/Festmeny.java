02fde4665e62ce6aff7fc49121faf8c3db516fc9
Varga <vargazsigmond02@gmail.com>
2022-09-22 08:33:20 +0200

    osztalyok

--- src/hu/petrik/aukcio/Festmeny.java
+++ src/hu/petrik/aukcio/Festmeny.java
@@ -15,8 +15,41 @@ public class Festmeny {
         this.cim = cim;
         this.festo = festo;
         this.stilus = stilus;
-        this.licitekSzama=0;
-        this.legmagasabbLicit=0;
-        this.elkelt=false;
+        this.licitekSzama = 0;
+        this.legmagasabbLicit = 0;
+        this.elkelt = false;
     }
+
+    public String GetFesto() {
+        return this.festo;
+    }
+
+    public String GetStilus() {
+        return this.stilus;
+    }
+
+    public int GetLicitekSzama() {
+        return this.licitekSzama;
+    }
+
+    public int GetLegmagasabbLicit() {
+        return this.legmagasabbLicit;
+    }
+
+    public LocalDateTime GetLegutolsoLicitIdeje() {
+        return this.legutolsoLicitIdeje;
+    }
+
+    public boolean GetElkelt() {
+        return this.elkelt;
+    }
+
+    public void setElkelt(boolean elkelt) {
+        this.elkelt = true;
+    }
+
+    public void licit(int mertek) {
+
+    }
+
 }
